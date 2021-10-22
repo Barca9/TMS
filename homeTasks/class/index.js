cb1f256ba4d9369c6f8df5091a8d51fa1eed2db8
Anastasia Kovalenok <nastassia.kovalenok@gmail.com>
2021-10-22 12:35:20 +0300

    added solution 4th task

--- homeTasks/class/index.js
+++ homeTasks/class/index.js
@@ -18,9 +18,9 @@ class Programmer{
     }
 }
 
-const p1 = new Programmer("Nastya", ["Python", "PHP", "Java"])
+const pr1 = new Programmer("Nastya", ["Python", "PHP", "Java"])
 
-p1.code(p1.stack)
+pr1.code(pr1.stack)
 
 //----------2-----------------------------------------------------------------------------
 
@@ -28,23 +28,66 @@ Programmer.prototype.toString = function () {
     console.log("Hello may name is Oleg")
 }
 
-p1.toString()
+pr1.toString()
 
 //---------3------------------------------------------------------------------------------
 
-/*
-const p2 = new Programmer("Nastya", ["Python", "PHP", "Java"]);
-const p3 = new Programmer("Nastya", ["Python", "PHP", "Java"]);
-const p4 = new Programmer("Nastya", ["Python", "PHP", "Java", "Scala"]);
-const p5 = new Programmer("Nastya", ["Python", "PHP", "Java"]);
-const p6 = new Programmer("Nastya", ["Python", "PHP", "Java"]);
-const p7 = new Programmer("Nastya", ["Python", "PHP", "Java", "Kotlin", "JS"]);
-const p8 = new Programmer("Nastya", ["Python", "PHP", "Java"]);
-const p9 = new Programmer("Nastya", ["Python", "PHP", "Java", "C++", "C#"]);
+const pr2 = new Programmer("Nastya", ["Python", "PHP", "Java"]);
+const pr3 = new Programmer("Nastya", ["Python", "PHP", "Java"]);
+const pr4 = new Programmer("Nastya", ["Python", "PHP", "Java", "Scala"]);
+const pr5 = new Programmer("Nastya", ["Python", "PHP", "Java"]);
+const pr6 = new Programmer("Nastya", ["Python", "PHP", "Java"]);
+const pr7 = new Programmer("Nastya", ["Python", "PHP", "Java", "Kotlin", "JS"]);
+const pr8 = new Programmer("Nastya", ["Python", "PHP", "Java"]);
+const pr9 = new Programmer("Nastya", ["Python", "PHP", "Java", "C++", "C#"]);
 
-const arrObject = [p2,p3,p4,p5,p6,p7,p8,p9];
+const arrObject = [pr2,pr3,pr4,pr5,pr6,pr7,pr8,pr9];
 
 let resArr = arrObject.filter(item => item.stack.length>4)
 
-console.log(resArr)*/
+console.log(resArr)
 
+//--------4--------------------------------------------------------------------------------
+
+//возможен ли треугольник
+//теорема пифагора  гипотенуза кв = катет кв. + катет кв.
+//сумма 2ух любых сторон больше чем сумма 3ей стороны (любой)
+//класс Point
+// Line ===>> Point1 Point2
+// function getLength()
+// function canTriangleExist()
+
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+const p1 = new Point(21, 51)
+const p2 = new Point(44, 83)
+
+class Line {
+    constructor(p1, p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+}
+
+const l1 = new Line(p1, p2);
+const l2 = new Line(new Point(43, 52), new Point(50, 90));
+const l3 = new Line(new Point(79, 31), new Point(23, 78));
+
+function getLength({p1, p2}) {
+    return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2))
+}
+
+function canTriangleExist(length1, length2, length3) {
+
+    return (length1 + length2 > length3
+        && length2 + length3 > length1
+        && length3 + length1 > length2);
+}
+
+console.log(canTriangleExist(getLength(l1), getLength(l2), getLength(l3)));
